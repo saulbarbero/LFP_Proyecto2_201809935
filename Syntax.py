f4b917050e5efcc734dade2fce0920b95a9cb5a9
saulbarbero <82236139+saulbarbero@users.noreply.github.com>
2021-10-28 18:24:58 -0600

    Match

--- Syntax.py
+++ Syntax.py
@@ -14,3 +14,13 @@ class Syntax:
         self.Parametro()
         self.match(self.tokens[self.n].lexema,")")
         self.match(self.tokens[self.n].lexema,";")
+
+    def match(self,token, token_esperado):
+        print(str(token),":",str(token_esperado))
+        if(self.error):
+            if( token != token_esperado):
+                if(self.tokens[self.n].lexema=="$"):
+                    self.error = False
+                e = Error("Error sintactico:"+str(token),0,0)
+                self.lista_errores.append(e)
+        self.n+=1
