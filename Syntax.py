425c806140e08cd69198989094926f8c73a16138
saulbarbero <82236139+saulbarbero@users.noreply.github.com>
2021-10-28 18:36:24 -0600

    Analizar

--- Syntax.py
+++ Syntax.py
@@ -8,12 +8,54 @@ class Syntax:
         self.n = 0
         self.error = True
 
+    def Analizar(self):
+        t= Token(PR.SYMBOL,"$",0,0)
+        self.tokens.append(t)
+        while self.tokens[self.n].lexema != None:
+            if(self.tokens[self.n].lexema=="$"):
+                break
+            self.match(self.tokens[self.n].token,PR.ID)
+            
+            if self.tokens[self.n].lexema == "(":
+                self.Llamada_funcion()
+            elif self.tokens[self.n].lexema == "=":
+                self.match(self.tokens[self.n].lexema,"=")
+
+                self.match(self.tokens[self.n].lexema,"[")
+                #if self.tokens[self.n].lexema == "{":
+                    #Registro()
+                #else:
+                    #Parametro_clave()
+            else:
+                self.error = False
+                e = Error("Error sintactico:"+self.tokens[self.n].lexema,0,0)
+                self.lista_errores.append(e)
+                break
+                #Error
+
 
     def Llamada_funcion(self):
         self.match(self.tokens[self.n].lexema,"(")
         self.Parametro()
         self.match(self.tokens[self.n].lexema,")")
         self.match(self.tokens[self.n].lexema,";")
+
+    def Parametro (self):
+        if(self.tokens[self.n].token == PR.CADENA):
+           self.match(self.tokens[self.n].token,PR.CADENA)
+        elif(self.tokens[self.n].token == PR.NUM):
+           self.match(self.tokens[self.n].token,PR.NUM)
+        elif(self.tokens[self.n].token == PR.DECI):
+           self.match(self.tokens[self.n].token,PR.DECI)
+        else:
+            self.error = False
+            e = Error("Error sintactico:"+self.tokens[self.n].lexema,0,0)
+            self.lista_errores.append(e)
+
+
+        if(self.tokens[self.n].lexema == ","):
+                self.match(self.tokens[self.n].lexema,",")
+                self.Parametro()
 
     def match(self,token, token_esperado):
         print(str(token),":",str(token_esperado))
